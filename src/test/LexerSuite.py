7dee129355964b3d3270940dabefc73a141d456a
HuyDNA <huydo862003@gmail.com>
2024-01-22 20:42:48 +0700

    test: add number tests

--- src/test/LexerSuite.py
+++ src/test/LexerSuite.py
@@ -22,3 +22,28 @@ class LexerSuite(unittest.TestCase):
     def test_valid_escape(self):
         """test valid escape"""
         self.assertTrue(TestLexer.test("\"abc \\f\"", "\"abc \\f\",<EOF>", 105))
+    
+    def test_integer(self):
+        """test integer"""
+        self.assertTrue(TestLexer.test("123", "123,<EOF>", 106))
+
+    def test_float_with_no_decimal(self):
+        """test floating point number with no decimal"""
+        self.assertTrue(TestLexer.test("123.", "123.,<EOF>", 107))
+    
+    def test_float_with_decimal(self):
+        """test floating point number with a decimal part"""
+        self.assertTrue(TestLexer.test("123.456", "123.456,<EOF>", 108))
+
+    def test_invalid_float_without_int(self):
+        """test invalid floating point number without an integral part"""
+        self.assertTrue(TestLexer.test(".456", "Error Token .", 109))
+
+    def test_float_with_no_decimal_and_with_exp(self):
+        """test valid floating point number without a decimal part but have an exponent part"""
+        self.assertTrue(TestLexer.test("123.e456", "123.e456,<EOF>", 110))
+        self.assertTrue(TestLexer.test("123e456", "123e456,<EOF>", 111))
+    
+    def test_full_float(self):
+        """test full fledge floating point number"""
+        self.assertTrue(TestLexer.test("123.456e789", "123.456e789,<EOF>", 112))
