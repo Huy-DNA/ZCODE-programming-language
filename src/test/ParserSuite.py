f25580d23fa0f937065123c55ed57da08a04e3b2
HuyDNA <huydo862003@gmail.com>
2024-01-26 23:27:59 +0700

    test: add more function test

--- src/test/ParserSuite.py
+++ src/test/ParserSuite.py
@@ -2,9 +2,31 @@ import unittest
 from TestUtils import TestParser
 
 class ParserSuite(unittest.TestCase):
-    def test_simple_program(self):
-        """Simple program: int main() {} """
+    def test_return_function(self):
+        """Function with simple return body"""
         input = """func main () return 1
         """
         expect = "successful"
         self.assertTrue(TestParser.test(input,expect,201))
+    
+    def test_block_function(self):
+        """Function with body as block"""
+        input = """func main ()
+        begin
+            return 1 + 2
+        end
+        """
+        expect = "successful"
+        self.assertTrue(TestParser.test(input,expect,202))
+    
+    def test_block_function_with_comment(self):
+        """Function with body as block"""
+        input = """func main () ## This is the main function
+        ## This is before the body
+        begin
+            ## This is the body
+            return "abc'""
+        end
+        """
+        expect = "successful"
+        self.assertTrue(TestParser.test(input,expect,203))
