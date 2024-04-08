24590bfb347a592d859cbac67af6843c8ad70ddf
HuyDNA <huydo862003@gmail.com>
2024-04-08 09:15:16 +0700

    fix: fn in CheckerResult

--- src/main/zcode/checker/StaticCheck.py
+++ src/main/zcode/checker/StaticCheck.py
@@ -86,10 +86,10 @@ class CheckerParam:
         self.lookupKind = lookupKind
 
 class CheckerResult:
-    def __init__(self, typ, scope, fn = None, isLvalue = False):
+    def __init__(self, typ, scope, fnType = None, isLvalue = False):
         self.type = typ
         self.scope = scope
-        self.fn = None
+        self.fnType = fnType
         self.isLvalue = isLvalue
 
 def isSameType(type1, type2):
