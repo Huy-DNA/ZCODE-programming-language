f31b82a20faa4b920c1af101ca10ab217abb813f
Huy-DNA <huydo862003@gmail.com>
2024-05-08 15:15:31 +0700

    fix: runtime errors

--- src/main/zcode/codegen/CodeGenerator.py
+++ src/main/zcode/codegen/CodeGenerator.py
@@ -126,7 +126,7 @@ class CodeGenVisitor(BaseVisitor):
         name = ast.name.name
         scope = param.scope
         in_, _ = scope.lookup(name, Function())
-        self.emit.printout(self.emit.emitMETHOD(name, in_))
+        self.emit.printout(self.emit.emitMETHOD(name, in_, param.frame))
         param.frame.enterScope(True)
         self.emit.printout(self.emit.emitLABEL(param.frame.getStartLabel(), param.frame))
         for paramDecl in ast.param:
