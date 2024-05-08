5492710be14b2c013392b494251051c608692fbd
Huy-DNA <huydo862003@gmail.com>
2024-05-08 09:17:10 +0700

    fix: runtime errors

--- src/main/zcode/codegen/CodeGenerator.py
+++ src/main/zcode/codegen/CodeGenerator.py
@@ -94,7 +94,7 @@ class CodeGenVisitor(BaseVisitor):
         self.emit = Emitter(path + "/" +self.classname + ".j")
 
     def visitProgram(self, ast, c):
-        self.emit.emitPROLOG(self.classname)
+        self.emit.emitPROLOG(self.classname, "")
         c = SubBody(None, self.env, ast.scope)
         [self.visit(i, c) for i in ast.decl]
         self.emit.emitEPILOG()
